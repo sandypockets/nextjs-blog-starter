a075f779b84ef791669ebdd5b7f18fe9cda57789
sandypockets <zac@znacreative.com>
2021-08-24 17:04:38 -0400

    Restructure contact form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -5,6 +5,7 @@ import { BLOG_NAME } from "../lib/constants";
 import Layout from "../components/Layout/Layout";
 import Container from "../components/Layout/Container";
 import Intro from "../components/Home/Intro";
+import ContactPage from "../components/Utils/ContactForm/ContactPage";
 
 export default function Contact() {
   const [message, setMessage] = useState('')
@@ -30,15 +31,23 @@ export default function Contact() {
         <title>Contact | {BLOG_NAME}</title>
       </Head>
       <Container>
-        <Intro />
-        <h1>This is the Contact page</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            <span>Message:</span>
-            <textarea onChange={handleInput} value={message} />
-          </label>
-          <button type="submit">submit</button>
-        </form>
+        <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+          <h1 className="text-6xl md:text-8xl lg:text-9xl font-bold tracking-tighter leading-tight md:pr-8">
+            Let's connect.
+          </h1>
+        </section>
+
+        {/*<h1>This is the Contact page</h1>*/}
+        {/*<form onSubmit={handleSubmit}>*/}
+        {/*  <label>*/}
+        {/*    <span>Message:</span>*/}
+        {/*    <textarea onChange={handleInput} value={message} />*/}
+        {/*  </label>*/}
+        {/*  <button type="submit">submit</button>*/}
+        {/*</form>*/}
+
+        <ContactPage />
+
       </Container>
     </Layout>
   )
