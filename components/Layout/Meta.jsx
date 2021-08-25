442da9e16c07126fe78c8da6a7153bacfc3091f9
sandypockets <zac@znacreative.com>
2021-08-25 01:13:47 -0400

    Tidy code

--- components/Layout/Meta.jsx
+++ components/Layout/Meta.jsx
@@ -7,11 +7,10 @@ export default function Meta() {
     <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        {/*<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />*/}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-            rel="stylesheet"
-          />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
         <link
         rel="apple-touch-icon"
         sizes="180x180"
