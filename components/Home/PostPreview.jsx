c1a81c723cabc005479c37d6f111c5e2ac97d63a
sandypockets <zac@znacreative.com>
2021-08-25 00:58:28 -0400

    Reduce bottom margin

--- components/Home/PostPreview.jsx
+++ components/Home/PostPreview.jsx
@@ -22,7 +22,7 @@ export default function PostPreview({
           width={556}
         />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="text-3xl lg:text-4xl mb-1 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
