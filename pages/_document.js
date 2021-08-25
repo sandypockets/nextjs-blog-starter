d9cb5543f148b7ad93b41c69a3932203b394d96a
sandypockets <zac@znacreative.com>
2021-08-25 16:26:42 -0400

    Remove hardcoded dark class

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import { GA_TRACKING_ID } from '../lib/gtag'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang={LANG} className="dark">
+      <Html lang={LANG}>
         <Head>
           <meta name="description" content={DESCRIPTION} />
           <meta name="author" content={AUTHOR} />
