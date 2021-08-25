e2d6e9456ac4b8c075d8bae96c9016dd62030f8b
sandypockets <zac@znacreative.com>
2021-08-25 14:34:11 -0400

    Improve performance - Set image download as priority

--- components/Post/PostHeader.jsx
+++ components/Post/PostHeader.jsx
@@ -14,7 +14,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
           <DateFormatter dateString={date} />
         </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} height={620} width={1240} />
+        <CoverImage title={title} src={coverImage} height={620} width={1240} coverImagePriority={true} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
