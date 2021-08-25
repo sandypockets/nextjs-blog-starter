9d453bd10a52378b6f7a33a12d1d2430299db40c
sandypockets <zac@znacreative.com>
2021-08-24 22:44:58 -0400

    Remove dead code

--- components/Utils/ContactForm/ContactPage.jsx
+++ components/Utils/ContactForm/ContactPage.jsx
@@ -5,9 +5,6 @@ export default function ContactPage() {
 
   return (
     <div className="relative bg-white dark:bg-black">
-      {/*<div className="absolute inset-0">*/}
-      {/*  <div className="absolute inset-y-0 left-0 w-1/2 " />*/}
-      {/*</div>*/}
       <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
         <ContactDetails />
         <ContactForm />
