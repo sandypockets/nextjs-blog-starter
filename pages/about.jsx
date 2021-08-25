154f9e80c832856ef907e434e19717b91488029c
sandypockets <zac@znacreative.com>
2021-08-24 22:41:36 -0400

    Add breakpoints

--- pages/about.jsx
+++ pages/about.jsx
@@ -15,7 +15,7 @@ export default function About() {
             Nice to meet you.
           </h1>
         </section>
-        <section className="max-w-2xl mx-auto py-24">
+        <section className="max-w-4xl mx-auto py-8 md:py-16 lg:py-24">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <br/>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
