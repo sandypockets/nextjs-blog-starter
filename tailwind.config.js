6c942c6467ba5c52bb6b88c84d29b72baa422fd9
sandypockets <zac@znacreative.com>
2021-12-15 17:55:41 -0500

    Upgrade to Tailwind CSS 3.0.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
@@ -21,28 +21,9 @@ module.exports = {
         // Transparent and current
         transparent: 'transparent',
         current: 'currentColor',
-        // All available colors. Remove as needed.
-        blueGray: colors.blueGray,
-        coolGray: colors.coolGray,
-        gray: colors.gray,
-        trueGray: colors.trueGray,
-        warmGray: colors.warmGray,
-        red: colors.red,
-        orange: colors.orange,
-        amber: colors.amber,
-        yellow: colors.yellow,
-        lime: colors.lime,
-        green: colors.green,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        sky: colors.sky,
-        blue: colors.blue,
-        indigo: colors.indigo,
-        violet: colors.violet,
-        purple: colors.purple,
-        fuchsia: colors.fuchsia,
-        pink: colors.pink,
-        rose: colors.rose,
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
       },
       spacing: {
         28: '7rem',
