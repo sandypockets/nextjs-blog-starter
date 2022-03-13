a3a7ba1d241cbfa7e4b31bb73175cab36e1e1e2c
sandypockets <zac@znacreative.com>
2022-03-13 12:09:00 -0400

    Prettier refactor

--- components/Image/Avatar.jsx
+++ components/Image/Avatar.jsx
@@ -3,7 +3,13 @@ import Image from 'next/image'
 export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
-      <Image src={picture} height={48} width={48} className="w-12 h-12 rounded-full" alt={name} />
+      <Image
+        src={picture}
+        height={48}
+        width={48}
+        className="w-12 h-12 rounded-full"
+        alt={name}
+      />
       <div className="text-xl font-bold ml-4">{name}</div>
     </div>
   )
