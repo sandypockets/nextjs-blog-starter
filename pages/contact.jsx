7fc4bcd82506e6f61f1cd27412b4d2b6fae12000
sandypockets <zac@znacreative.com>
2022-03-13 13:07:48 -0400

    Remove duplicate code

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import Head from 'next/head'
-import * as gtag from '../lib/gtag'
 import { BLOG_NAME } from '../lib/constants'
 import Layout from '../components/Layout/Layout'
 import Container from '../components/Layout/Container'
@@ -8,23 +6,6 @@ import ContactPage from '../components/Utils/ContactForm/ContactPage'
 import PageHeading from '../components/PageHeading'
 
 export default function Contact() {
-  const [message, setMessage] = useState('')
-
-  const handleInput = (e) => {
-    setMessage(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    gtag.event({
-      action: 'submit_form',
-      category: 'Contact',
-      label: message,
-      value: message,
-    })
-    setMessage('')
-  }
-
   return (
     <Layout>
       <Head>
