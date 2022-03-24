b9c2c7753a68625a56f2763abe34eb533815f776
sandypockets <zac@znacreative.com>
2022-03-23 21:45:30 -0400

    Remove duplicate tag

--- components/Layout/Meta.jsx
+++ components/Layout/Meta.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../../lib/constants'
+import { HOME_OG_IMAGE_URL } from '../../lib/constants'
 
 // Favicon should be recreated at various sizes for each link below.
 export default function Meta() {
@@ -32,10 +32,7 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
+
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   )
